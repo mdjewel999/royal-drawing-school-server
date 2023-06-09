73cd9055ffb846f88ad02f2ec818c3282cf324a6
mdjewel999 <mdjewel878761@gmail.com>
2023-06-09 14:01:56 +0600

    menu section code added

--- index.js
+++ index.js
@@ -26,12 +26,33 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const menuCollection = client.db("drawingDb").collection("menu");
+    const reviewCollection = client.db("drawingDb").collection("reviews");
+
+
+     // menu related apis
+     app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+
+
+     // review related apis
+     app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
