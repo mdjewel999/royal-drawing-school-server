2e12e50fbda492462b37b8d228e9b5c2f4f44087
mdjewel999 <mdjewel878761@gmail.com>
2023-06-11 13:17:03 +0600

    put UpdateOn

--- index.js
+++ index.js
@@ -146,7 +146,12 @@ async function run() {
       res.send(result);
     })
 
-
+    app.put('/menu/:id', async(req, res) => {
+      const id = req.params.id; 
+      const query= {_id: new ObjectId(id)}
+    const result = await menuCollection.updateOne(query)
+      res.send(result);
+    });
 
 
      // review related apis
