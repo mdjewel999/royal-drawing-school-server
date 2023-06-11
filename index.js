44bacf30b1b212aae49b23dc3822fc0d9fc6658a
mdjewel999 <mdjewel878761@gmail.com>
2023-06-11 11:40:46 +0600

    delete code update

--- index.js
+++ index.js
@@ -139,12 +139,12 @@ async function run() {
       res.send(result);
     })
 
-    // app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) }
-    //   const result = await menuCollection.deleteOne(query);
-    //   res.send(result);
-    // })
+    app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
 
