f8a83c3dd4a86653972958721342511eb12a05e3
mdjewel999 <mdjewel878761@gmail.com>
2023-06-11 11:31:53 +0600

    delete

--- index.js
+++ index.js
@@ -10,22 +10,22 @@ app.use(cors());
 app.use(express.json());
 
 
-// const verifyJWT = (req, res, next) => {
-//   const authorization = req.headers.authorization;
-//   if (!authorization) {
-//     return res.status(401).send({ error: true, message: 'unauthorized access' });
-//   }
-//   // bearer token
-//   const token = authorization.split(' ')[1];
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
 
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//     if (err) {
-//       return res.status(401).send({ error: true, message: 'unauthorized access' })
-//     }
-//     req.decoded = decoded;
-//     next();
-//   })
-// }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 
@@ -60,16 +60,16 @@ async function run() {
     })
 
 
-    // // Warning: use verifyJWT before using verifyAdmin
-    // const verifyAdmin = async (req, res, next) => {
-    //   const email = req.decoded.email;
-    //   const query = { email: email }
-    //   const user = await usersCollection.findOne(query);
-    //   if (user?.role !== 'admin') {
-    //     return res.status(403).send({ error: true, message: 'forbidden message' });
-    //   }
-    //   next();
-    // }
+    // Warning: use verifyJWT before using verifyAdmin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'forbidden message' });
+      }
+      next();
+    }
 
 
 
@@ -122,11 +122,30 @@ async function run() {
 
     })
 
+
+
+
      // menu related apis
      app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
+
+
+    // verifyJWT, verifyAdmin,
+    app.post('/menu',verifyJWT, verifyAdmin, async (req, res) => {
+      const newItem = req.body;
+      const result = await menuCollection.insertOne(newItem)
+      res.send(result);
+    })
+
+    // app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) }
+    //   const result = await menuCollection.deleteOne(query);
+    //   res.send(result);
+    // })
+
 
 
 
@@ -138,7 +157,8 @@ async function run() {
 
 
     
-    // cart collection apis// verifyJWT,
+    // cart collection apis   verifyJWT,
+
     app.get("/carts", async (req, res) => {
       const email = req.query.email;
 
